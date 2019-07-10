6f807c50b37bf9ce1d2cb45e8714fa9ff176b8ac
apizarras <angelalee.p@gmail.com>
2019-07-10 17:54:33 -0500

    wrapper

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import components
+//import Card from "./components/..."
+import smurfs from "./smurfs.json"
+
+
+class App extends Component {
+  state = {
+    smurfs
+  };
+
+  removeSmurf = id => {
+    const smurfs = this.state.smurfs.filter(smurf => smurf.id !== id);
+    this.setState({smurfs});
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Smurfs List</Title>
+        {this.state.smurfs.map(smurf=> (
+          <SmurfCard
+            removeSmurf={this.removeSmurf}
+            id={smurf.id}
+            key={smurf.id}
+            name={smurf.name}
+            image={smurf.image}
+            occupation={smurf.occupation}
+            location={smurf.location}
+            />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
